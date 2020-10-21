b6e0b06c206f0fe9457c4821b4c213ce1ea13287
Sneha Gupta <44.sneha.gupta.446@gmail.com>
2020-10-21 18:15:56 +0530

    Update PredictingPercentOfStudent.py

--- PredictingPercentOfStudent.py
+++ PredictingPercentOfStudent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[55]:
+
 
 
 import pandas as pd
@@ -12,85 +12,37 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 style.use('ggplot')
-
-
-# In[4]:
-
-
 url = "http://bit.ly/w-data"
 df = pd.read_csv(url)
 df.head()
-
-
 # Plotting the data
-
-# In[18]:
-
-
 df.plot(x="Scores", y="Hours", style="o")
 plt.title("Hours vs Pecentage")
 plt.xlabel("Scores")
 plt.ylabel("Hours")
 plt.show()
-
-
 # From the above plot, it is clearly visible that hours and scores have linear relationship. 
 # So the further prediction could be done using 
 # Linear Regression Algorithm
-
-# In[36]:
-
-
 X = df.iloc[:, :-1].values  
 y = df.iloc[:, 1].values  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 clf = LinearRegression()
 clf.fit(X_train, y_train)
 clf.score(X_test, y_test)
-
-
 # # plotting the Linear Regression Model
-
-# In[39]:
-
-
 line = clf.coef_*X+clf.intercept_
 plt.scatter(X, y, color='g')
 plt.plot(X, line)
 plt.show
-
-
 # # Predicting the Data
-
-# In[46]:
-
-
 X_test
 y_predict = clf.predict(X_test)
-
-
-# In[49]:
-
-
 df1 = pd.DataFrame({'Actual' : y_test, 'Predicted': y_predict})
 df1.head()
-
-
 # # Checking the Accuracy of Prediction
-
-# In[50]:
-
-
 clf.score(X_test, y_test)
-
-
 # It is evidently visible that our prediction is 94.54% accurate.
 # although it could be more accurate using different ML algorithms
-
 # # Evaluating the Model
-
-# In[56]:
-
-
 print("Mean Absolute Error is" , metrics.mean_absolute_error(y_test, y_predict))
-
